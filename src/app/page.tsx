8e06eacb917d3d83db3f797db1385cc20f85de0a
fawadmehmood1 <fawad.mehmood@stixor.com>
2025-07-07 14:00:33 +0500

    updated main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@
 import HeroSection from "@/src/components/home-page/hero-section";
 import AboutSection from "@/src/components/home-page/about-section";
 import ServicesSection from "@/src/components/home-page/services";
-import Banner from "@/src/components/home-page/banner";
+// import Banner from "@/src/components/home-page/banner";
 import DepartmentsSection from "@/src/components/home-page/departments";
-import ContactSection from "@/src/components/home-page/contact-section";
+// import ContactSection from "@/src/components/home-page/contact-section";
 import Team from "@/src/components/home-page/team";
 
 export default function Home() {
@@ -15,8 +15,8 @@ export default function Home() {
       <DepartmentsSection />
       <ServicesSection />
       <Team />
-      <ContactSection />
-      <Banner />
+      {/* <ContactSection /> */}
+      {/* <Banner /> */}
     </div>
   );
 }
