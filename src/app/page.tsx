d4e596ab1707695348c48470a69522597af93b29
fawadmehmood1 <fawad.mehmood@stixor.com>
2025-07-07 14:13:02 +0500

    removed unused component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,7 @@
 import HeroSection from "@/src/components/home-page/hero-section";
 import AboutSection from "@/src/components/home-page/about-section";
 import ServicesSection from "@/src/components/home-page/services";
-// import Banner from "@/src/components/home-page/banner";
 import DepartmentsSection from "@/src/components/home-page/departments";
-// import ContactSection from "@/src/components/home-page/contact-section";
 import Team from "@/src/components/home-page/team";
 
 export default function Home() {
@@ -15,8 +13,6 @@ export default function Home() {
       <DepartmentsSection />
       <ServicesSection />
       <Team />
-      {/* <ContactSection /> */}
-      {/* <Banner /> */}
     </div>
   );
 }
